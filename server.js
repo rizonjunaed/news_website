6b2102cc356ff415fb328a9c8c56b3308c414da8
Md Junaed Hossain <hossain.junaed11@gmail.com>
2022-07-30 00:09:19 -0500

    Ensured internal CSS and JS works in localhost.

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const httpModule = require('http');
+const fileSystemModule = require('fs');
 
 const requestHandler = (req, res) => {
+    console.log(req.headers.host, req.url, 'request values');
+
     if (req.url !== '/'){
         res.statusCode = 400; 
-        res.end("Failed");
+        res.end();
     }
     else{
-        res.statusCode = 200;
-        res.end("Success");
+        const handleFileRead = (err, contents) => {
+            if (err)
+            {
+                res.statusCode = 400;
+
+                res.end();
+            }
+            else {
+                res.statusCode = 200;
+
+                res.setHeader('Content-Type', 'text/html');
+
+                res.write(contents)
+
+                res.end();
+            }
+        }
+        
+        fileSystemModule.readFile('./news.html', handleFileRead); 
     }
 }
 
